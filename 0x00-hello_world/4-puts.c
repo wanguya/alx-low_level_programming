244d172900935dba947cc8738233d6b0414acd85
wanguya <wangiyajay@gmail.com>
2022-09-08 16:31:00 -0700

    fhjghjfh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  *main - entry point
  *
- * Return 0 after printing the function
+ * Return: 0 after printing the function (success)
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
