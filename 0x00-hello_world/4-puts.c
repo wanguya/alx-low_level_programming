6860578978b871f9e5c4865c0695fa0329c69d66
wanguya <wangiyajay@gmail.com>
2022-09-08 15:26:00 -0700

    bin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/** main - entry
+/**
+ *main - entry point
  *
- * return 0 after printing the function
+ * Return 0 after printing the function
  */
 int main(void)
-{puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
