8f4b2ba3b7b897462f77a0d1a0a160a4e8646d64
wanguya <wangiyajay@gmail.com>
2022-09-09 15:12:29 -0700

    hghgh

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
 			putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
