1aab2de60f4b4f76a43e023d40fe4f1f89656f99
wanguya <wangiyajay@gmail.com>
2022-09-10 10:40:18 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,16 @@
-include <stdio.h>
+#include<stdio.h>
 /**
-* main - printing numbers from 0-9 with commas and space between them
-* Description: using the main function
-* this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-* Return: 0
-*/
+ * main - entry point
+ *
+ * Return: always returns 0
+ */
 int main(void)
 {
-		int c;
-
-			for (c = 48; c <= 57; c++)
-					{
-								putchar(c);
-										if (c != 57)
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-											}
-				putchar('\n');
-					return (0);
+char a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+for (a = 'A'; a <= 'Z'; a++)
+putchar(a);
+putchar ('\n');
+return (0);
 }
