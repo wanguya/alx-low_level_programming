43dca0fa7d02385cec002a88c7441d20de6fa10e
wanguya <wangiyajay@gmail.com>
2022-09-15 00:56:32 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,8 @@ int print_last_digit(int n)
 {
 int last = n % 10;
 if (n < 0)
-{
-	last = last * -1;
-	_putchar(last + '0');
-	return (last);
-}
+last = last * -1;
+_putchar(last + '0');
+return (last);
 }
 
