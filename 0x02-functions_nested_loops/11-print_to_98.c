546c2b02f47f4db235339e7e491d761bd241b2c8
wanguya <wangiyajay@gmail.com>
2022-09-15 01:41:26 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,10 +7,10 @@
  */
 void print_times_table(int n)
 {
-	int a = 0, rep, b;
+int a = 0, rep, b;
 
-	if (n < 0 || n > 15)
-		return;
+if (n < 0 || n > 15)
+	return;
 	while (a <= n)
 	{
 		for (b = 0; b <= n; b++)
