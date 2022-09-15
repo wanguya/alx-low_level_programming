048daac5d803ca6daa8545b2f231bc3713ab7fdd
wanguya <wangiyajay@gmail.com>
2022-09-14 22:20:32 -0700

    jjjkjkjj

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+nclude "main.h"
+/**
+ * add - returns the sum of  the given parameters
+ * @a: int type number
+ * @b: int type number
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
