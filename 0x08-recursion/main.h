fca00f1173e6d7753575ea69dd6a3f465ba5fc34
wanguya <wangiyajay@gmail.com>
2022-09-28 13:42:57 -0700

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,16 +2,20 @@
 
 #define MAIN_H
 
-void _puts_recursion(char *s)
-void _print_rev_recursion(char *s)
-int _strlen_recursion(char *s)
 
-int str_checker(char *s1, char *s2, int i, int j)
-int _strlen_recursion(char *s)
-int factorial(int n)
-int _pow_recursion(int x, int y)
+#include <stdio.h>
+#include <stdlib.h>
 
-int find_root(int n, int i)
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
-int find_multipliers(int n, int i)	
+int str_checker(char *s1, char *s2, int i, int j);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+
+int find_root(int n, int i);
+
+int find_multipliers(int n, int i);	
 #endif/*MAIN_H*/
